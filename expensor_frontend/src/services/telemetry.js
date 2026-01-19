32f8aa5add4d3d8096412078b5593e5867620abe
Gi0NZ <g.nigro32@studenti.unisa.it>
2026-01-19 13:46:54 +0100

    fix ; telemetry

--- expensor_frontend/src/services/telemetry.js
+++ expensor_frontend/src/services/telemetry.js
@@ -2,7 +2,7 @@ import { ApplicationInsights } from '@microsoft/applicationinsights-web';
 import { ReactPlugin } from '@microsoft/applicationinsights-react-js';
 
 const reactPlugin = new ReactPlugin();
-const connString = process.env.REACT_APP_INSIGHT_CONNECTION_STRING
+const connString = process.env.REACT_APP_INSIGHT_CONNECTION_STRING;
 const appInsights = new ApplicationInsights({
     config: {
         connectionString: connString,
